22c4fb47f5243e4c22c7aad65b7972d4db0acb1a
IanDesktop <ian.laga@montana.edu>
2025-10-15 09:46:02 -0600

    Stylized file

--- R/fit_stan_optim.R
+++ R/fit_stan_optim.R
@@ -1,4 +1,3 @@
-
 library(cmdstanr)
 
 
@@ -14,57 +13,61 @@ library(cmdstanr)
 #' @export
 #'
 #' @examples
-fit_stan_optim = function(ard,
-                    x_cov_global = NULL,
-                    x_cov_local = NULL,
-                    cov_names = NULL,
-                    family = c("poisson", "nbinomial"),
-                    ...) {
+fit_stan_optim <- function(ard,
+                           x_cov_global = NULL,
+                           x_cov_local = NULL,
+                           cov_names = NULL,
+                           family = c("poisson", "nbinomial"),
+                           ...) {
   ## Grab family
-  family = match.arg(family, c("poisson", "nbinomial"))
-  
-  n_local = ncol(x_cov_local)
-  n_global = ncol(x_cov_global)
-  
-  if(is.null(x_cov_global) & is.null(x_cov_local)){ # No cov
-    stan_data = list(
+  family <- match.arg(family, c("poisson", "nbinomial"))
+
+  n_local <- ncol(x_cov_local)
+  n_global <- ncol(x_cov_global)
+
+  if (is.null(x_cov_global) & is.null(x_cov_local)) {
+    # No cov
+    stan_data <- list(
       y = ard,
       n_i = nrow(ard),
       n_k = ncol(ard)
     )
-    if(family == "poisson"){
-      mod = cmdstan_model("./Stan_Files/Poisson.stan")
-    }else{
-      mod = cmdstan_model("./Stan_Files/Overdispersed.stan")
+    if (family == "poisson") {
+      mod <- cmdstan_model("./Stan_Files/Poisson.stan")
+    } else {
+      mod <- cmdstan_model("./Stan_Files/Overdispersed.stan")
     }
-  }else if(is.null(x_cov_global)){ # Only subpop cov
-    stan_data = list(
+  } else if (is.null(x_cov_global)) {
+    # Only subpop cov
+    stan_data <- list(
       y = ard,
       n_i = nrow(ard),
       n_k = ncol(ard),
       z_subpop_size = n_local,
       z_subpop = x_cov_local
     )
-    if(family == "poisson"){
-      mod = cmdstan_model("./Stan_Files/Poisson_zsubpop.stan")
-    }else{
-      mod = cmdstan_model("./Stan_Files/Overdispersed_zsubpop.stan")
+    if (family == "poisson") {
+      mod <- cmdstan_model("./Stan_Files/Poisson_zsubpop.stan")
+    } else {
+      mod <- cmdstan_model("./Stan_Files/Overdispersed_zsubpop.stan")
     }
-  }else if(is.null(x_cov_local)){ # Only global cov
-    stan_data = list(
+  } else if (is.null(x_cov_local)) {
+    # Only global cov
+    stan_data <- list(
       y = ard,
       n_i = nrow(ard),
       n_k = ncol(ard),
       z_global_size = n_global,
       z_global = x_cov_global
     )
-    if(family == "poisson"){
-      mod = cmdstan_model("./Stan_Files/Poisson_zglobal.stan")
-    }else{
-      mod = cmdstan_model("./Stan_Files/Overdispersed_zglobal.stan")
+    if (family == "poisson") {
+      mod <- cmdstan_model("./Stan_Files/Poisson_zglobal.stan")
+    } else {
+      mod <- cmdstan_model("./Stan_Files/Overdispersed_zglobal.stan")
     }
-  }else{ # Both types of covariates
-    stan_data = list(
+  } else {
+    # Both types of covariates
+    stan_data <- list(
       y = ard,
       n_i = nrow(ard),
       n_k = ncol(ard),
@@ -73,30 +76,26 @@ fit_stan_optim = function(ard,
       z_global_size = n_global,
       z_global = x_cov_global
     )
-    if(family == "poisson"){
-      mod = cmdstan_model("./Stan_Files/Poisson_zglobal_zsubpop.stan")
-    }else{
-      mod = cmdstan_model("./Stan_Files/Overdispersed_zglobal_zsubpop.stan")
+    if (family == "poisson") {
+      mod <- cmdstan_model("./Stan_Files/Poisson_zglobal_zsubpop.stan")
+    } else {
+      mod <- cmdstan_model("./Stan_Files/Overdispersed_zglobal_zsubpop.stan")
     }
   }
-  
-  
 
-  
-  
-  fit = mod$optimize(data = stan_data, ...)
-  
-  
+
+
+
+
+  fit <- mod$optimize(data = stan_data, ...)
+
+
   ## Add residuals
   if (family == "poisson") {
     pois_lambda_est <- fit$summary(variables = "mu")$estimate
     alphas <- fit$summary(variables = "alphas")$estimate
-    resid_vec <- construct_pearson(data.frame(value = c(ard)),
-                                   family = "poisson",
-                                   fit = pois_lambda_est)
-    resids <- matrix(resid_vec,
-                     nrow = nrow(ard),
-                     ncol = ncol(ard))
+    resid_vec <- construct_pearson(data.frame(value = c(ard)), family = "poisson", fit = pois_lambda_est)
+    resids <- matrix(resid_vec, nrow = nrow(ard), ncol = ncol(ard))
   } else if (family == "nbinomial") {
     nb_prob_est <- fit$summary(variables = "inv_omegas")$estimate
     nb_size_est <- fit$summary(variables = "par1")$estimate
@@ -107,14 +106,14 @@ fit_stan_optim = function(ard,
       size = nb_size_est,
       prob = rep(nb_prob_est, each = n_i)
     )
-    resids <- matrix(resid_vec,
-                     nrow = nrow(ard),
-                     ncol = ncol(ard))
+    resids <- matrix(resid_vec, nrow = nrow(ard), ncol = ncol(ard))
   }
-  
-  return_obj = list(fit = fit,
-                    family = family,
-                    residuals = resids)
-  
+
+  return_obj <- list(
+    fit = fit,
+    family = family,
+    residuals = resids
+  )
+
   return(return_obj)
 }
