f11a905e4719cb8a2f52fc0746359343aae8c5e8
IanDesktop <ian.laga@montana.edu>
2025-10-20 10:20:23 -0600

    Updated covariate functionality and formatting to add cleaned covariate names on right-side of global plot.

--- R/cov_plots.R
+++ R/cov_plots.R
@@ -2,8 +2,12 @@ library(ggplot2)
 
 #' Title
 #'
-#' @param ard
-#' @param x_cov
+#'
+#' @param model_fit 
+#' @param ard 
+#' @param x_cov 
+#' @param method 
+#' @param se 
 #'
 #' @return
 #' @export
@@ -12,62 +16,88 @@ library(ggplot2)
 cov_plots <- function(model_fit,
                       ard,
                       x_cov,
-                      cov_names = NULL,
+                      method = "lm",
                       se = F) {
-  
+  ## Grab family
   family <- model_fit$family
-  
+
   ## Obtain residuals
-  resid_mat = model_fit$residuals
+  resid_mat <- model_fit$residuals
   alpha_est <- model_fit$fit$summary(variables = "alphas")$estimate
 
-  ## Convert x_cov to data.frame, if not already
+  ## Convert ard to data.frame, if not already
   if (!inherits(ard, "data.frame")) {
     ard <- data.frame(ard)
   }
-  
+
   ## Convert x_cov to data.frame, if not already
   if (!inherits(x_cov, "data.frame")) {
-    x_cov <- data.frame(x_cov)
+    x_cov <- data.frame(x_cov) ## Auto-names to X1:Xp if no names
   }
-  
-  if (is.null(cov_names)) {
-    cov_names <- paste0("cov.", names(x_cov))
-  }
-  
+
   ard_x <- data.frame(resid = resid_mat, cov = x_cov, alpha = alpha_est)
   ard_long <- ard_x %>%
-    pivot_longer(cols = starts_with("resid."),
-                 names_to = "Group",
-                 values_to = "resid")
-  # ard_long$resid <- resid_vec
-  
-  
+    pivot_longer(
+      cols = starts_with("resid."),
+      names_to = "Group",
+      values_to = "resid"
+    )
+
+
   ard_longer <- ard_long %>%
-    pivot_longer(cols = starts_with("cov."),
-                 names_to = "CovariateName",
-                 values_to = "CovValue")
-  
-  ## Produce plot 1, averaged over groups
+    pivot_longer(
+      cols = starts_with("cov."),
+      names_to = "cov_names",
+      values_to = "CovValue"
+    ) %>%
+    mutate(cov_label = str_remove(cov_names, "^cov\\."))
+
+
+  ## Produce plot 1, group-specific plots
   gg1 <- ggplot(ard_longer, aes(
     x = CovValue,
     y = resid,
     col = Group,
     group = Group
   )) +
-    geom_smooth(method = "lm", se = se) +
-    # geom_smooth(se = se) +
-    facet_wrap( ~ CovariateName, scales = "free") +
+    geom_smooth(method = method, se = se) +
+    facet_wrap(~cov_label, scales = "free") +
     theme_minimal() +
     labs(x = "Covariate Value", y = "Residual", title = "Residuals vs Covariates") +
     scale_color_discrete(guide = "none")
-  
-  gg2 <- ggplot(ard_longer, aes(x = CovValue, y = alpha, col = CovariateName)) +
-    # geom_point(alpha = 0.6) +
-    geom_smooth(method = "lm") +
-    # facet_wrap(~ CovariateName, scales = "free_x") +
+
+
+  ## Find endpoints to add covariate label
+  label_df <- ard_longer %>%
+    split(.$cov_label) %>%
+    imap_dfr(~ {
+      sm <- ggplot_build(
+        ggplot(.x, aes(x = CovValue, y = alpha)) +
+          geom_smooth(method = method, se = se)
+      )$data[[1]]
+
+      sm %>%
+        filter(x == max(x, na.rm = TRUE)) %>%
+        mutate(cov_label = .y)
+    })
+
+  # Produce plot 2, averaged over groups
+  gg2 <- ggplot(ard_longer, aes(x = CovValue, y = alpha, color = cov_label)) +
+    geom_smooth(method = method, se = se) +
+    geom_text(
+      data = label_df,
+      aes(x = x, y = y, label = cov_label, color = cov_label),
+      hjust = -0.1,
+      show.legend = FALSE
+    ) +
+    expand_limits(x = max(label_df$x) * 1.1) +
     theme_minimal() +
-    labs(x = "Covariate Value", y = "Estimated Respondent Effect", title = "Respondent Effect vs Covariate")
-  
+    labs(
+      x = "Covariate Value",
+      y = "Estimated Respondent Effect",
+      title = "Respondent Effect vs Covariate",
+      color = "Covariate"
+    )
+
   return(list(Group_plot = gg1, Respondent_plot = gg2))
 }
