ed1377f19de15ee02b11fcae46382ce81e0d414e
IanDesktop <ian.laga@montana.edu>
2025-11-08 10:20:59 -0700

    Added numerically stable negative binomial call.

--- R/aux_functions.R
+++ R/aux_functions.R
@@ -129,22 +129,8 @@ construct_rqr <- function(y, model_fit = NULL,
     }
   } else if (family == "nbinomial") {
     for (i in 1:length(y_vec)) {
-      # Get CDF at y[i] and at y[i] - 1
-      F_lower <- NA
-      if (y_vec[i] == 0) {
-        F_lower <- 0
-      } else {
-        F_lower <- stats::pnbinom(y_vec[i] - 1, size = size_vec[i],
-                                  prob = prob_vec[i])
-      }
-      F_upper <- stats::pnbinom(y_vec[i], size = size_vec[i],
-                                prob = prob_vec[i])
-      
-      # Sample a uniform value between F_lower and F_upper
-      u <- stats::runif(1, min = F_lower, max = F_upper)
-      
-      # Inverse standard normal transformation
-      rqr[i] <- stats::qnorm(u)
+      ## to avoid some numerical issues
+      rqr[i] <- rqr_nbinom_logs(y_vec[i], size_vec[i], prob_vec[i])
     }
   } else if (family == "poisson") {
     for (i in 1:length(y_vec)) {
@@ -297,6 +283,24 @@ log_mix_uniform <- function(logFl, logFu) {
 rqr_pois_logs <- function(y, mu, eps = 1e-12) {
   logFu <- stats::ppois(y, mu, log.p = TRUE)
   logFl <- stats::ppois(y - 1, mu, log.p = TRUE)
+  logu  <- log_mix_uniform(logFl, logFu)
+  # Clip in probability space *after* exponentiating
+  u     <- base::exp(logu)
+  u     <- base::pmin(base::pmax(u, eps), 1 - eps)
+  stats::qnorm(u)
+}
+
+#' compute numerically stable negative binomial rqr
+#'
+#' @param y observed value
+#' @param size size parameter
+#' @param prob prob parameter
+#' @param eps precision parameter
+#'
+#' @returns appropriate randomized quantile residual
+rqr_nbinom_logs <- function(y, size, prob, eps = 1e-12) {
+  logFu <- stats::pnbinom(y, size = size, prob = prob, log.p = TRUE)
+  logFl <- stats::pnbinom(y - 1, size = size, prob = prob, log.p = TRUE)
   logu  <- log_mix_uniform(logFl, logFu)
   # Clip in probability space *after* exponentiating
   u     <- base::exp(logu)
